27c02d5f38de2a736fb3270fc0031362fb210cbc
Christopher Fu <chrisf1337@gmail.com>
2016-12-29 23:25:45 +0900

    Add tests for insertion into last half

--- gapbuffer/tests/gapbuffer_tests.rs
+++ gapbuffer/tests/gapbuffer_tests.rs
@@ -77,3 +77,57 @@ fn test_insert_at_pt4() {
     s = (&gb.buf[gb.gap_end_idx..]).iter().cloned().collect::<String>();
     assert_eq!(s, "");
 }
+
+#[test]
+/// Insertion into last half of gap
+fn test_insert_at_pt5() {
+    let mut gb = GapBuffer::with_capacity(10);
+    let mut chars = "abc".chars().collect::<Vec<char>>();
+    &gb.buf[..chars.len()].copy_from_slice(&chars[..]);
+    gb.gap_start_idx += chars.len();
+
+    chars = "de".chars().collect::<Vec<char>>();
+    let buf_len = gb.buf.len();
+    &gb.buf[buf_len - chars.len()..].copy_from_slice(&chars[..]);
+    gb.gap_end_idx -= chars.len();
+    gb.text_len = 5;
+
+    gb.insert_at_pt("fg", 4);
+    // First half is "abcdfg", second half is "e"
+    assert_eq!(gb.gap_start_idx, 6);
+    assert_eq!(gb.gap_end_idx, 9);
+    assert_eq!(gb.point_idx, 6);
+    assert_eq!(gb.text_len, 7);
+
+    let mut s = (&gb.buf[..gb.gap_start_idx]).iter().cloned().collect::<String>();
+    assert_eq!(s, "abcdfg");
+    s = (&gb.buf[gb.gap_end_idx..]).iter().cloned().collect::<String>();
+    assert_eq!(s, "e");
+}
+
+#[test]
+/// Complete displacement of last half
+fn test_insert_at_pt6() {
+    let mut gb = GapBuffer::with_capacity(10);
+    let mut chars = "abc".chars().collect::<Vec<char>>();
+    &gb.buf[..chars.len()].copy_from_slice(&chars[..]);
+    gb.gap_start_idx += chars.len();
+
+    chars = "de".chars().collect::<Vec<char>>();
+    let buf_len = gb.buf.len();
+    &gb.buf[buf_len - chars.len()..].copy_from_slice(&chars[..]);
+    gb.gap_end_idx -= chars.len();
+    gb.text_len = 5;
+
+    gb.insert_at_pt("fg", 5);
+    // First half is "abcdfg", second half is "e"
+    assert_eq!(gb.gap_start_idx, 7);
+    assert_eq!(gb.gap_end_idx, 10);
+    assert_eq!(gb.point_idx, 7);
+    assert_eq!(gb.text_len, 7);
+
+    let mut s = (&gb.buf[..gb.gap_start_idx]).iter().cloned().collect::<String>();
+    assert_eq!(s, "abcdefg");
+    s = (&gb.buf[gb.gap_end_idx..]).iter().cloned().collect::<String>();
+    assert_eq!(s, "");
+}
