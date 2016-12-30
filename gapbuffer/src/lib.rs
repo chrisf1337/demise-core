31d0253574ea813b396b4a9134574ee1c57f2c68
Christopher Fu <chrisf1337@gmail.com>
2016-12-30 21:34:13 +0900

    Set gap indices to buf.len() when full; add tests

--- gapbuffer/src/lib.rs
+++ gapbuffer/src/lib.rs
@@ -30,6 +30,10 @@ impl GapBuffer {
     }
 
     fn move_gap(&mut self, point: usize) {
+        if self.gap_start_idx == self.buf.len() {
+            // Return if there is no gap
+            return;
+        }
         if point < self.gap_start_idx {
             // Move chars on and after point to last half
             let mut moved_chars_copy = vec!['\0'; self.gap_start_idx - point];
@@ -51,6 +55,20 @@ impl GapBuffer {
         assert_eq!(point, self.gap_start_idx);
     }
 
+    /// Inserts the string at the point. If the point is not the gap start
+    /// index, we move the gap by copying all characters in the current half to
+    /// the other half so that the new gap start index is at point
+    /// (see move_gap()).
+    ///
+    /// Inserting at point p results in the first char of string to be located
+    /// at p, and the point will be updated to p + string.len() (as will the gap
+    /// start index).
+    ///
+    /// If we insert a string that is longer than the gap length, the buffer
+    /// will be doubled successively in size until the string can fit.
+    ///
+    /// If we insert a string that is exactly the same length as the gap, the
+    /// gap start and end indexes will be updated to buf.len().
     pub fn insert_at_pt(&mut self, string: &str, point: usize) {
         let mut gap_len = self.gap_end_idx - self.gap_start_idx;
         if string.len() > gap_len {
@@ -85,10 +103,18 @@ impl GapBuffer {
         }
         self.move_gap(point);
         let string_chars = string.to_string().chars().collect::<Vec<char>>();
-        let copy_slice = &mut self.buf[self.gap_start_idx..self.gap_start_idx + string_chars.len()];
-        copy_slice.copy_from_slice(&string_chars);
+        {
+            let copy_slice = &mut self.buf[self.gap_start_idx..self.gap_start_idx + string_chars.len()];
+            copy_slice.copy_from_slice(&string_chars);
+        }
         self.gap_start_idx += string_chars.len();
         self.point_idx = self.gap_start_idx;
         self.text_len += string_chars.len();
+
+        // No more gap if gap_start_idx == gap_end_idx
+        if self.gap_start_idx == self.gap_end_idx {
+            self.gap_start_idx = self.buf.len();
+            self.gap_end_idx = self.buf.len();
+        }
     }
 }
--- gapbuffer/tests/gapbuffer_tests.rs
+++ gapbuffer/tests/gapbuffer_tests.rs
@@ -3,7 +3,7 @@ use gapbuffer::GapBuffer;
 
 #[test]
 /// Simple insertion into empty gap buffer
-fn test_insert_at_pt1() {
+fn test_insert_at_pt_simple1() {
     let mut gb = GapBuffer::with_capacity(10);
     gb.insert_at_pt("abc", 0);
     assert_eq!(gb.gap_start_idx, 3);
@@ -17,7 +17,7 @@ fn test_insert_at_pt1() {
 
 #[test]
 /// Insertion into start of gap buffer with existing text
-fn test_insert_at_pt2() {
+fn test_insert_at_pt_simple2() {
     let mut gb = GapBuffer::with_capacity(10);
     let chars = "abc".chars().collect::<Vec<char>>();
     &gb.buf[..chars.len()].copy_from_slice(&chars[..]);
@@ -38,7 +38,7 @@ fn test_insert_at_pt2() {
 
 #[test]
 /// Insertion into middle of first half of gap buffer with existing text
-fn test_insert_at_pt3() {
+fn test_insert_at_pt_simple3() {
     let mut gb = GapBuffer::with_capacity(10);
     let chars = "abc".chars().collect::<Vec<char>>();
     &gb.buf[..chars.len()].copy_from_slice(&chars[..]);
@@ -59,7 +59,7 @@ fn test_insert_at_pt3() {
 
 #[test]
 /// Insertion into beginning of gap
-fn test_insert_at_pt4() {
+fn test_insert_at_pt_simple4() {
     let mut gb = GapBuffer::with_capacity(10);
     let chars = "abc".chars().collect::<Vec<char>>();
     &gb.buf[..chars.len()].copy_from_slice(&chars[..]);
@@ -80,7 +80,7 @@ fn test_insert_at_pt4() {
 
 #[test]
 /// Insertion into last half of gap
-fn test_insert_at_pt5() {
+fn test_insert_at_pt_simple5() {
     let mut gb = GapBuffer::with_capacity(10);
     let mut chars = "abc".chars().collect::<Vec<char>>();
     &gb.buf[..chars.len()].copy_from_slice(&chars[..]);
@@ -107,7 +107,7 @@ fn test_insert_at_pt5() {
 
 #[test]
 /// Complete displacement of last half
-fn test_insert_at_pt6() {
+fn test_insert_at_pt_simple6() {
     let mut gb = GapBuffer::with_capacity(10);
     let mut chars = "abc".chars().collect::<Vec<char>>();
     &gb.buf[..chars.len()].copy_from_slice(&chars[..]);
@@ -130,4 +130,76 @@ fn test_insert_at_pt6() {
     assert_eq!(s, "abcdefg");
     s = (&gb.buf[gb.gap_end_idx..]).iter().cloned().collect::<String>();
     assert_eq!(s, "");
+}
+
+#[test]
+/// Add until full
+fn test_insert_at_pt_simple7() {
+    let mut gb = GapBuffer::with_capacity(10);
+    let chars = "abc".chars().collect::<Vec<char>>();
+    &gb.buf[..chars.len()].copy_from_slice(&chars[..]);
+    gb.gap_start_idx += chars.len();
+    gb.text_len = 3;
+
+    gb.insert_at_pt("0123456", 0);
+    assert_eq!(gb.buf.len(), 10);
+    assert_eq!(gb.gap_start_idx, 10);
+    assert_eq!(gb.gap_end_idx, 10);
+    assert_eq!(gb.point_idx, 7);
+    assert_eq!(gb.text_len, 10);
+
+    let s = (&gb.buf[..gb.gap_start_idx]).iter().cloned().collect::<String>();
+    assert_eq!(s, "0123456abc");
+}
+
+#[test]
+/// Resize once
+fn test_insert_at_pt_resize1() {
+    let mut gb = GapBuffer::with_capacity(10);
+    let mut chars = "abc".chars().collect::<Vec<char>>();
+    &gb.buf[..chars.len()].copy_from_slice(&chars[..]);
+    gb.gap_start_idx += chars.len();
+
+    chars = "de".chars().collect::<Vec<char>>();
+    let buf_len = gb.buf.len();
+    &gb.buf[buf_len - chars.len()..].copy_from_slice(&chars[..]);
+    gb.gap_end_idx -= chars.len();
+    gb.text_len = 5;
+
+    gb.insert_at_pt("01234567", 0);
+    assert_eq!(gb.buf.len(), 20);
+    assert_eq!(gb.gap_start_idx, 8);
+    assert_eq!(gb.gap_end_idx, 15);
+    assert_eq!(gb.point_idx, 8);
+    assert_eq!(gb.text_len, 13);
+
+    let mut s = (&gb.buf[..gb.gap_start_idx]).iter().cloned().collect::<String>();
+    assert_eq!(s, "01234567");
+
+    s = (&gb.buf[gb.gap_end_idx..]).iter().cloned().collect::<String>();
+    assert_eq!(s, "abcde");
+}
+
+#[test]
+/// Resize twice when full
+fn test_insert_at_pt_resize2() {
+    let mut gb = GapBuffer::with_capacity(5);
+    let chars = "abcde".chars().collect::<Vec<char>>();
+    &gb.buf[..chars.len()].copy_from_slice(&chars[..]);
+    gb.gap_start_idx = chars.len();
+    gb.gap_end_idx = chars.len();
+    gb.text_len = 5;
+
+    gb.insert_at_pt("01234567", 0);
+    assert_eq!(gb.buf.len(), 20);
+    assert_eq!(gb.gap_start_idx, 8);
+    assert_eq!(gb.gap_end_idx, 15);
+    assert_eq!(gb.point_idx, 8);
+    assert_eq!(gb.text_len, 13);
+
+    let mut s = (&gb.buf[..gb.gap_start_idx]).iter().cloned().collect::<String>();
+    assert_eq!(s, "01234567");
+
+    s = (&gb.buf[gb.gap_end_idx..]).iter().cloned().collect::<String>();
+    assert_eq!(s, "abcde");
 }
